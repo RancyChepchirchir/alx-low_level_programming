997fad25c5865a1f8a94b636f67aa0639242799d
RancyChepchirchir <kosgeyrancy2@gmail.com>
2022-09-13 19:08:12 +0300

    addition of 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_sign - prints the sign of a number
- *@n: number being tested
- * Return: Always 0.
+ * print_sign - Print the sign of a number
+ * @n: type int integer, can be negative or positive
+ * Description: print +, 0, or - depending on number, along with return
+ * Return: 1 if +, 0 if 0, and -1 if -
  */
 
 int print_sign(int n)
@@ -13,15 +13,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
 	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
-	return (0);
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
