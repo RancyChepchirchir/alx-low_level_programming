e11e86fd3d32f7fda0f3174680b68f8da2894f74
RancyChepchirchir <kosgeyrancy2@gmail.com>
2022-09-13 19:45:28 +0300

    addition of 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,12 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * add - adds two integers and returns the result
- * @a: an integer to add to b
- * @b: an integer to add to a
- * Return: Void.
+ * add - add two numbers from input
+ * @a: first aparamet
+ * @b: second parameter
+ *
+ * Description: adds two numbers
+ * Return: Always (0).
  */
 
 int add(int a, int b)
